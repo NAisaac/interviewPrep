544f1f92da713af6bc282fa570861625622c86b8
isaacyi <iyi@researchinnovations.com>
2018-02-19 21:11:47 -0500

    not a complete tree yet

--- js/tree.js
+++ js/tree.js
@@ -58,3 +58,71 @@ Tree.prototype.traverseBF = function (callback) {
     currentNode = bfsQueue.dequeue();
   }
 };
+
+// contains() - search for value in the tree
+Tree.prototype.contains = function (callback, traversal) {
+  traversal.call(this, callback);
+};
+
+
+// add() - add a node to a specific node
+Tree.prototype.add = function (data, toData, traversal) {
+  var errorMessage = 'Failure: no such node to add to';
+  var child = new Node(data);
+  var parent = null;
+  var callback = function (node) {
+    if (node.data === toData) {
+      parent = node;
+    }
+  };
+
+  this.contains(callback, traversal);
+
+  if (parent) {
+    parent.children.push(child);
+    child.parent = parent;
+  } else {
+    console.error(errorMessage);
+    throw new Error(errorMessage);
+  }
+};
+
+// remove() - remove a node from a speicific node
+Tree.prototype.remove = function (data, fromData, traversal) {
+  var parent = null,
+      childToRemove = null,
+      index;
+  var callback = function (node) {
+    if (node.data === fromData) {
+      parent = node;
+    }
+  };
+
+  function findIndex(arr, data) {
+    var index;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].data === data) {
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  this.contains(callback, traversal);
+
+  if (parent) {
+    index = findIndex(parent.children, data);
+
+    if (!index) {
+      console.error('Node to remove DNE');
+      throw new Error('Node to remove DNE');
+    } else {
+      childToRemove = parent.children.splice(index, 1);
+    }
+  } else {
+    console.error('Node to remove from DNE');
+    throw new Error('Node to remove from DNE');
+  }
+
+  return childToRemove;
+};
